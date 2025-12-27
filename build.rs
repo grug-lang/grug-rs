77bbb7573e762e1584b4c673306e1d8b2237528d
NikhilNathanael <nikhilnathanael2020@gmail.com>
2025-12-27 15:49:55 +0530

    pulled latest grug-tests which now uses cmake and runs on windows modified build.rs to work with newest grug-tests

--- build.rs
+++ build.rs
@@ -10,47 +10,50 @@ fn build_tests() {
 
 	let out_dir = PathBuf::from(var("OUT_DIR").unwrap());
 
-	let source_path = String::from("./src/grug-tests");
+	let source_path = String::from("./src/grug-tests/build/");
 	let archive_path = String::from(var("OUT_DIR").unwrap());
 
+	println!("{:?}", source_path);
+	println!("{:?}", std::fs::canonicalize(source_path.clone()));
 	println!("{:?}", archive_path);
 
-	if std::fs::exists(&archive_path).ok().is_some_and(|x| !x) {
-		#[cfg(target_os = "linux")]
-		{
-			match std::fs::copy(&source_path + "tests.so", &archive_path + "libtests.so") {
-				Ok(_) => (),
-				Err(_) => println!(
-					"grug-tests is not pulled yet\n\
-					Run the following commands first\n\n\
-					'git submodule update --init --force'\n\
-					'cd src/grug-tests/'\n\
-					'./build.sh'"
-				),
-			}
+	#[cfg(target_os = "linux")]
+	{
+		match std::fs::copy(source_path + "/libtests.so", archive_path + "/libtests.so") {
+			Ok(_) => (),
+			Err(_) => println!(
+				"test"
+				// "grug-tests is not pulled yet\n\
+				// Run the following commands first\n\n\
+				// 'git submodule update --init --force'\n\
+				// 'cd src/grug-tests/'\n\
+				// './build.sh'"
+			),
 		}
-		#[cfg(target_os = "windows")]
-		{
-			match std::fs::copy(source_path.clone() + "/tests.dll", archive_path.clone() + "/tests.dll") {
-				Ok(_) => (),
-				Err(_) => println!(
-					"grug-tests is not pulled yet\n\
-					Run the following commands first\n\n\
-					'git submodule update --init --force'\n\
-					'cd src/grug-tests/'\n\
-					'./build.sh'"
-				),
-			}
-			match std::fs::copy(source_path + "/libtests.dll.a", archive_path + "/tests.lib") {
-				Ok(_) => (),
-				Err(_) => println!(
-					"grug-tests is not pulled yet\n\
-					Run the following commands first\n\n\
-					'git submodule update --init --force'\n\
-					'cd src/grug-tests/'\n\
-					'./build.sh'"
-				),
-			}
+	}
+	#[cfg(target_os = "windows")]
+	{
+		match std::fs::copy(source_path.clone() + "tests.dll", archive_path.clone() + "/tests.dll") {
+			Ok(_) => (),
+			Err(_) => println!(
+				""
+				// "grug-tests is not pulled yet\n\
+				// Run the following commands first\n\n\
+				// 'git submodule update --init --force'\n\
+				// 'cd src/grug-tests/'\n\
+				// './build.sh'"
+			),
+		}
+		match std::fs::copy(source_path + "libtests.dll.a", archive_path + "/tests.lib") {
+			Ok(_) => (),
+			Err(_) => panic!(
+				""
+				// "grug-tests is not pulled yet\n\
+				// Run the following commands first\n\n\
+				// 'git submodule update --init --force'\n\
+				// 'cd src/grug-tests/'\n\
+				// './build.sh'"
+			),
 		}
 	}
 	println!("cargo::rustc-link-search={}", out_dir.display());
