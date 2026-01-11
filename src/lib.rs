d5bff55ce21a841ee025b677a0ed7567528c2771
NikhilNathanael <nikhilnathanael2020@gmail.com>
2026-01-11 19:52:00 +0530

    turned grug_test into an integration test in the test/ directory moved crate::bindings to a module within tests/grug_tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 // #![deny(warnings)]
 
-pub mod bindings;
 pub mod frontend;
 pub mod backend;
 
@@ -9,53 +8,6 @@ pub mod types;
 pub mod serde;
 pub mod error;
 pub mod state;
-
-#[cfg(test)]
-mod test {
-	use super::bindings::*;
-	use std::ffi::CString;
-	use std::mem::ManuallyDrop;
-	use crate::mod_api::*;
-	use crate::state::GrugState;
-
-	#[test] 
-	fn grug_tests () {
-		let mut args = std::env::args().collect::<Vec<_>>();
-
-		let mut whitelisted_test = std::ptr::null();
-		if args.len() == 3 {
-			whitelisted_test = ManuallyDrop::new(CString::new(args.pop().unwrap()).unwrap()).as_ptr();
-		} else if args.len() > 3 {
-			eprintln!("usage: cargo test -- grug_tests <whitelisted_test>");
-			std::process::exit(2);
-		}
-		// let grug_path = &*args[1];
-		// println!("{}", grug_path);
-
-		// let 
-
-		let grug_tests_path = c"src/grug-tests/tests";
-		let mod_api_text = std::fs::read_to_string("src/grug-tests/mod_api.json").unwrap();
-
-		let state = GrugState::new("src/grug-tests/mod_api.json", grug_tests_path.to_str().unwrap()).unwrap();
-		// register_game_functions(&mut state);
-			
-		_ = GLOBAL_TEST_STATE.set(state);
-
-		unsafe {
-			grug_tests_run(
-				grug_tests_path.as_ptr(),
-				compile_grug_file,
-				init_globals_fn_dispatcher,
-				on_fn_dispatcher,
-				dump_file_to_json,
-				generate_file_from_json,
-				game_fn_error,
-				whitelisted_test,
-			)
-		}
-	}
-}
 
 // TODO: implementation errors like too many statements in block should not be grug errors
 // TODO: Better Error handling
--- tests/grug_tests/main.rs
+++ tests/grug_tests/main.rs
@@ -1,11 +1,14 @@
-use std::ffi::c_float;
-#[cfg(test)]
-pub mod test_bindings {
+use std::ffi::{CString, c_float};
+use std::mem::ManuallyDrop;
+use grug_rs::mod_api::*;
+use grug_rs::state::GrugState;
+
+mod test_bindings {
 	use super::*;
-	use crate::state::GrugState;
-	use crate::types::{GrugValue};
-	use crate::frontend;
-	use crate::serde;
+	use grug_rs::state::GrugState;
+	use grug_rs::types::{GrugValue};
+	use grug_rs::frontend;
+	use grug_rs::serde;
 	use std::ffi::{c_char, CStr, CString};
 	use std::sync::OnceLock;
 	use std::mem::ManuallyDrop;
@@ -89,5 +92,42 @@ pub mod test_bindings {
 		);
 	}
 }
-#[cfg(test)]
-pub use test_bindings::*;
+use test_bindings::*;
+
+#[test]
+fn main () {
+	let mut args = std::env::args().collect::<Vec<_>>();
+
+	let mut whitelisted_test = std::ptr::null();
+	if args.len() == 3 {
+		whitelisted_test = ManuallyDrop::new(CString::new(args.pop().unwrap()).unwrap()).as_ptr();
+	} else if args.len() > 3 {
+		eprintln!("usage: cargo test -- grug_tests <whitelisted_test>");
+		std::process::exit(2);
+	}
+	// let grug_path = &*args[1];
+	// println!("{}", grug_path);
+
+	// let 
+
+	let grug_tests_path = c"src/grug-tests/tests";
+	let mod_api_text = std::fs::read_to_string("src/grug-tests/mod_api.json").unwrap();
+
+	let state = GrugState::new("src/grug-tests/mod_api.json", grug_tests_path.to_str().unwrap()).unwrap();
+	// register_game_functions(&mut state);
+		
+	_ = GLOBAL_TEST_STATE.set(state);
+
+	unsafe {
+		grug_tests_run(
+			grug_tests_path.as_ptr(),
+			compile_grug_file,
+			init_globals_fn_dispatcher,
+			on_fn_dispatcher,
+			dump_file_to_json,
+			generate_file_from_json,
+			game_fn_error,
+			whitelisted_test,
+		)
+	}
+}
