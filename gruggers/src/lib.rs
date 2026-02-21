0382cb167d80d193d55a48edd105d46a5363dea9
NikhilNathanael <nikhilnathanael2020@gmail.com>
2026-02-22 00:37:52 +0530

    removed bench function in lib.rs

--- gruggers/src/lib.rs
+++ gruggers/src/lib.rs
@@ -48,8 +48,3 @@ mod cachemap;
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-
-#[bench]
-fn bench(bencher: &mut Bencher) -> () {
-	println!("Hello World");
-}
