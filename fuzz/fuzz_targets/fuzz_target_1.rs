da06f7356151a8aafb14a29a648435ad79ada64c
NikhilNathanael <nikhilnathanael2020@gmail.com>
2026-03-26 00:34:41 +0530

    fixed warning in fuzz

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -29,6 +29,6 @@ thread_local! {
 fuzz_target!(|data: &str| {
 	STATE.with_borrow_mut(|state: &mut Option<GrugState>| {
 		let state = state.get_or_insert_with(|| GrugState::new_from_text(MOD_API, "mods", Default::default(), BytecodeBackend::new()).unwrap());
-		state.compile_grug_file_from_str("test/test-A.grug", data);
+		_ = state.compile_grug_file_from_str("test/test-A.grug", data);
 	});
 });
